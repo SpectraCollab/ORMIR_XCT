281932d9e06668f5d4d7e4265941340a812a97fc
Nathan <njneeteson@gmail.com>
2022-11-02 13:02:25 -0600

    add structure for tests

---
+++ tests/util/test_hildebrand_thickness.py
@@ -0,0 +1,28 @@
+import unittest
+
+from ormir_xct.util.hildebrand_thickness import (
+    compute_local_thickness_from_distance_ridge,
+    compute_local_thickness_from_mask,
+    calc_structure_thickness_statistics
+)
+
+
+class TestComputeLocalThicknessFromDistanceRidge(unittest.TestCase):
+
+    @unittest.skip("unimplemented")
+    def test_placeholder(self):
+        pass
+
+
+class TestComputeLocalThicknessFromMask(unittest.TestCase):
+
+    @unittest.skip("unimplemented")
+    def test_placeholder(self):
+        pass
+
+
+class TestCalcStructureThicknessStatistics(unittest.TestCase):
+
+    @unittest.skip("unimplemented")
+    def test_placeholder(self):
+        pass
