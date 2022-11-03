fixed distance transform and distance ridge calculation

ormir_xct/util/hildebrand_thickness.py
@@ -12,7 +12,7 @@ from __future__ import annotations
 import numpy as np
 from collections.abc import Iterable
 from numba import jit
-from SimpleITK import GetImageFromArray, GetArrayFromImage, DanielssonDistanceMap
+from SimpleITK import BinaryThinning, GetImageFromArray, GetArrayFromImage, SignedMaurerDistanceMap, DanielssonDistanceMap
 from skimage.morphology import skeletonize_3d
 from typing import Union
 import warnings
@@ -60,6 +60,7 @@ def compute_local_thickness_from_distance_ridge(
     np.ndarray
         The local thickness field.
     """
+    """
     for (rd, (ri, rj, rk)) in zip(dist_ridge, dist_ridge_indices):
         for di in range(
             np.maximum(np.floor(ri - rd / voxel_width[0]) - 1, 0),
@@ -82,6 +83,17 @@ def compute_local_thickness_from_distance_ridge(
                         + (voxel_width[1] * (dj - rj)) ** 2
                         + (voxel_width[2] * (dk - rk)) ** 2
                     ) < (rd**2):
+                        local_thickness[di, dj, dk] = 2 * rd
+    """
+    for (rd, (ri, rj, rk)) in zip(dist_ridge, dist_ridge_indices):
+        for di in range(0, local_thickness.shape[0]):
+            for dj in range(0, local_thickness.shape[1]):
+                for dk in range(0, local_thickness.shape[2]):
+                    if (
+                        (voxel_width[0] * (di - ri)) ** 2
+                        + (voxel_width[1] * (dj - rj)) ** 2
+                        + (voxel_width[2] * (dk - rk)) ** 2
+                    ) <= (rd ** 2):
                         local_thickness[di, dj, dk] = 2 * rd
     return local_thickness
 
@@ -123,14 +135,14 @@ def compute_local_thickness_from_mask(
     else:
         raise ValueError("`voxel_width must be a float, int, or iterable of length 3`")
 
-    mask_sitk = GetImageFromArray(1 - mask.astype(int))
+    mask_sitk = GetImageFromArray((~mask).astype(int))
     mask_sitk.SetSpacing(tuple(voxel_width))
     mask_dist = mask * GetArrayFromImage(
-        DanielssonDistanceMap(mask_sitk, useImageSpacing=True, squaredDistance=False)
+        SignedMaurerDistanceMap(mask_sitk, useImageSpacing=True, insideIsPositive=False, squaredDistance=False)
     )
     ridge = [
         (mask_dist[i, j, k], i, j, k)
-        for (i, j, k) in zip(*(skeletonize_3d(mask).nonzero()))
+        for (i, j, k) in zip(*(GetArrayFromImage(BinaryThinning(GetImageFromArray(mask.astype(int)))).nonzero()))
     ]
     ridge.sort()
     ridge = np.asarray(ridge)