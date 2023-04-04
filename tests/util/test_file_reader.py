15e075a6dcc835f00429923edb82a985aa5e3d41
Michael Kuczynski <mkuczyns@ucalgary.ca>
2023-04-04 11:24:57 -0600

    Added headers and minor fixes

--- tests/util/test_file_reader.py
+++ tests/util/test_file_reader.py
@@ -1,9 +1,70 @@
-import unittest
+"""
+test_file_reader.py
 
-from ormir_xct.util.file_reader import file_reader 
+Created by:   Michael Kuczynski
+Created on:   June 29, 2022
+
+Description: Test the file reader.
+"""
+
+import os
+import unittest
+import SimpleITK as sitk
+
+from ormir_xct.util.file_reader import file_reader
+
 
 class TestFileReader(unittest.TestCase):
 
-    @unittest.skip("unimplemented")
-    def test_placeholder(self):
-        pass
+    filenames = [
+        "test_aim",
+        "test_isq",
+        "test_nii",
+        "test_mha",
+        "test_nrrd",
+        "test_dicom",
+    ]
+
+    path = os.getcwd()
+    parent = os.path.dirname(path)
+    filepath = os.path.join(parent, "data")
+
+    def test_file_read_aim(self):
+        aim_path = os.path.join(self.filepath, self.filenames[0] + ".AIM")
+        self.assertTrue(os.path.isfile(aim_path))
+
+        aim_image = file_reader(aim_path)
+        expected = type(sitk.Image())
+        self.assertEqual(type(aim_image), expected)
+
+    def test_file_read_isq(self):
+        isq_path = os.path.join(self.filepath, self.filenames[1] + ".ISQ")
+        self.assertTrue(os.path.isfile(isq_path))
+
+        isq_image = file_reader(isq_path)
+        expected = type(sitk.Image())
+        self.assertEqual(type(isq_image), expected)
+
+    def test_file_read_nii(self):
+        nii_path = os.path.join(self.filepath, self.filenames[2] + ".nii")
+        self.assertTrue(os.path.isfile(nii_path))
+
+        nii_image = file_reader(nii_path)
+        expected = type(sitk.Image())
+        self.assertEqual(type(nii_image), expected)
+
+    def test_file_read_mha(self):
+        mha_path = os.path.join(self.filepath, self.filenames[3] + ".mha")
+        self.assertTrue(os.path.isfile(mha_path))
+
+        mha_image = file_reader(mha_path)
+        expected = type(sitk.Image())
+        self.assertEqual(type(mha_image), expected)
+
+    def test_file_read_nrrd(self):
+        nrrd_path = os.path.join(self.filepath, self.filenames[4] + ".nrrd")
+        self.assertTrue(os.path.isfile(nrrd_path))
+
+        nrrd_image = file_reader(nrrd_path)
+        expected = type(sitk.Image())
+        self.assertEqual(type(nrrd_image), expected)
--- tests/util/test_scanco_rescale.py
+++ tests/util/test_scanco_rescale.py
@@ -1,5 +1,13 @@
-import unittest
+"""
+test_scanco_rescale.py
 
+Created by:   Michael Kuczynski
+Created on:   June 29, 2022
+
+Description: Test rescaling between image units.
+"""
+
+import unittest
 import numpy as np
 import SimpleITK as sitk
 
@@ -17,6 +25,7 @@ def create_test_image(dimensions, value):
     """
     array = np.full(dimensions, value)
     image = sitk.GetImageFromArray(array)
+    image = sitk.Cast(image, sitk.sitkFloat32)
     return image
 
 
@@ -27,42 +36,80 @@ class TestScancoRescale(unittest.TestCase):
         test_image = create_test_image((10, 10, 10), 8192)
         expected_image = create_test_image((10, 10, 10), 1)
         mu_scaling = 8192
-        result_image = convert_scanco_to_linear_attenuation(test_image)
+        result_image = convert_scanco_to_linear_attenuation(test_image, mu_scaling)
         np.testing.assert_array_equal(expected_image, result_image)
-        pass
 
     def test_scanco_to_hu(self):
         # 1. LinearAttenuation = ScancoUnits / mu_scaling
         # 2. HU = -1000 + LinearAttenuation * (1000 / mu_water)
-        pass
+        test_image = create_test_image((10, 10, 10), 8192)
+        expected_image = create_test_image((10, 10, 10), 1000)
+        mu_scaling = 8192
+        mu_water = 0.5
+        result_image = convert_scanco_to_hu(test_image, mu_scaling, mu_water)
+        np.testing.assert_array_equal(expected_image, result_image)
 
     def test_scanco_to_bmd(self):
         # 1. LinearAttenuation = ScancoUnits / mu_scaling
         # 2. BMD = LinearAttenuation * rescale_slope + rescale_intercept
-        pass
+        test_image = create_test_image((10, 10, 10), 8192)
+        expected_image = create_test_image((10, 10, 10), 10)
+        mu_scaling = 8192
+        rescale_slope = 9
+        rescale_intercept = 1
+        result_image = convert_scanco_to_bmd(test_image, mu_scaling, rescale_slope, rescale_intercept)
+        np.testing.assert_array_equal(expected_image, result_image)
 
     def test_hu_to_linear_attenuation(self):
         # LinearAttenuation = (HU + 1000) * (mu_water / 1000)
-        pass
+        test_image = create_test_image((10, 10, 10), 1000)
+        expected_image = create_test_image((10, 10, 10), 1)
+        mu_water = 0.5
+        result_image = convert_hu_to_linear_attenuation(test_image, mu_water)
+        np.testing.assert_array_equal(expected_image, result_image)
 
     def test_hu_to_scanco(self):
         # 1. LinearAttenuation = (HU + 1000) * (mu_water / 1000)
         # 2. ScancoUnits = LinearAttenuation * mu_scaling
-        pass
+        test_image = create_test_image((10, 10, 10), 1000)
+        expected_image = create_test_image((10, 10, 10), 8192)
+        mu_water = 0.5
+        mu_scaling = 8192
+        result_image = convert_hu_to_scanco(test_image, mu_water, mu_scaling)
+        np.testing.assert_array_equal(expected_image, result_image)
 
     def test_hu_to_bmd(self):
         # 1. LinearAttenuation = (HU + 1000) * (mu_water / 1000)
         # 2. BMD = LinearAttenuation * rescale_slope + rescale_intercept
-        pass
+        test_image = create_test_image((10, 10, 10), 1000)
+        expected_image = create_test_image((10, 10, 10), 2)
+        mu_water = 0.5
+        rescale_slope = 1
+        rescale_intercept = 1
+        result_image = convert_hu_to_bmd(test_image, mu_water, rescale_slope, rescale_intercept)
+        np.testing.assert_array_equal(expected_image, result_image)
 
     def test_linear_attenuation_to_hu(self):
         # HU = LinearAttenuation * (1000 / mu_water) - 1000
-        pass
+        test_image = create_test_image((10, 10, 10), 1)
+        expected_image = create_test_image((10, 10, 10), 1000)
+        mu_water = 0.5
+        result_image = convert_linear_attenuation_to_hu(test_image, mu_water)
+        np.testing.assert_array_equal(expected_image, result_image)
 
     def test_linear_attenuation_to_scanco(self):
         # ScancoUnits = LinearAttenuation * mu_scaling
-        pass
+        test_image = create_test_image((10, 10, 10), 1)
+        expected_image = create_test_image((10, 10, 10), 8192)
+        mu_scaling = 8192
+        result_image = convert_linear_attenuation_to_scanco(test_image, mu_scaling)
+        np.testing.assert_array_equal(expected_image, result_image)
 
     def test_linear_attenuation_to_bmd(self):
         # BMD = LinearAttenuation * rescale_slope + rescale_intercept
-        pass
+        test_image = create_test_image((10, 10, 10), 1)
+        expected_image = create_test_image((10, 10, 10), 8192)
+        rescale_slope = 8191
+        rescale_intercept = 1
+        result_image = convert_linear_attenuation_to_bmd(test_image, rescale_slope, rescale_intercept)
+        np.testing.assert_array_equal(expected_image, result_image)
